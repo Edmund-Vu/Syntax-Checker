4928a023d453d75281421a08a676f346425b59c5
Edmund <edmundvu816@gmail.com>
2020-04-04 04:29:56 +0000

    Wrote the main method, which loops if the user wants to add more files.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,27 @@
 #include "Assignment3.h"
 
 int main(int argc, char **argv){
-  delimiter d;
-  d.checkDelimiters();
+  GenStack<char> stack;
+  try{
+    delimiter d;
+    bool exit;
+    char c;
+    while(exit){
+      d.checkDelimiters();
+      cout << "Would you like to check another file? (Y/N)" << endl;
+      cin >> c;
+      if(c == 'N'){
+        exit = false;
+      }
+    }
+  }
+  catch(EmptyStackException e){
+    cout << e.what() << endl;
+  }
+  catch(FileNotFoundException e){
+    cout << e.what() << endl;
+  }
+  stack.~GenStack();
+  exit(0);
   return 0;
 }
